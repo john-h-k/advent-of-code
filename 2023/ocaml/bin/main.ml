ef93a5b1364e345db98b2fe999ac0d3a4afac90c
John Kelly <johnharrykelly@gmail.com>
2023-12-04 14:30:18 +0000

    ocaml day 3

--- 2023/ocaml/bin/main.ml
+++ 2023/ocaml/bin/main.ml
@@ -71,8 +71,8 @@ let () =
       let day_module = match day with
       | 1 -> (module Ocaml.Day1 : Day)
       | 2 -> (module Ocaml.Day2 : Day)
-      (*| 3 -> (module Ocaml.Day3 : Day)
-      | 4 -> (module Ocaml.Day4 : Day)*)
+      | 3 -> (module Ocaml.Day3 : Day)
+      (*| 4 -> (module Ocaml.Day4 : Day)*)
       | _ -> failwith (Printf.sprintf "day %d not implemented" day) in
 
       run_day day_module lines
